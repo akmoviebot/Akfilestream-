36de4bf68e00d246ab2807912cbfd1cba658984b
akmoviebot <mrak7753@gmail.com>
2024-12-19 18:49:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ else:
 URL = environ.get("URL", "")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002383459603')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002383459603'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7459282233').split()]
 
 # MongoDB information
